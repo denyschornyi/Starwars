06ada99322fcfd21f4af30ab6ac67e0a329176b5
Denys Chornyi <chornijj14@gmail.com>
2020-09-02 21:38:56 +0200

    Handle an error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const getRecource = async (url) => {
   const res = await fetch(url);
+
+  if(!res.ok){
+    throw new Error (`Could not fetch ${url}, recived ${res.status}`)
+  }
+
   const body = await res.json();
   return body;
 }
@@ -7,4 +12,7 @@ const getRecource = async (url) => {
 getRecource('http://swapi.dev/api/people/1/')
   .then(body => {
     console.log(body);
-});
+  })
+  .catch((err) => {
+    console.log(`Coud not fetch ${err}`);
+  });
