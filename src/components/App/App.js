d5bc86369b6fe5baf0c325ba5c7cea4d757a8cd2
Denys Chornyi <chornijj14@gmail.com>
2020-09-04 21:51:35 +0200

    Item list added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 import Header from '../Header';
-
+import ItemList from '../ItemList';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Header />
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+            {/* PersonDetails */}
+        </div>
+      </div>
     </div>
   );
 };
