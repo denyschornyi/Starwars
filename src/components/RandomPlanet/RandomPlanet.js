a35c2b2827352eaee02295bfd4150d7907297cef
Denys Chornyi <chornijj14@gmail.com>
2020-09-05 14:40:36 +0200

    Create state with info planet

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -4,25 +4,35 @@ import './RandomPlanet.css';
 
 export default class RandomPlanet extends Component {
 
+    state = {
+        name: null,
+        population:  null,
+        rotationPeriod: null,
+        diametr: null
+    }
+
   render() {
+
+    const {name, population, rotationPeriod, diametr} = this.state;
+
     return (
       <div className="random-planet jumbotron rounded">
         <img className="planet-image"
              src="https://starwars-visualguide.com/assets/img/planets/5.jpg"  alt='planet'/>
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>123124</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>43</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>100</span>
+              <span>{diametr}</span>
             </li>
           </ul>
         </div>
