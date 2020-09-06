10f23363ac1fbd3beb9dc24fa2cf3bbb3d648d26
Denys Chornyi <chornijj14@gmail.com>
2020-09-06 20:35:51 +0200

    Item List component now generated by connected to Api

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 
 import './ItemList.css';
 
+import SwapiService from '../../service/SwapiService'
+import Spiner from '../Spiner'
+
 export default class ItemList extends Component {
 
+  state = {
+    peopleList: null
+  }
+
+  renderItems(arr){
+    return arr.map(person => {
+      return(
+        <li key={person.id} className="list-group-item">
+          {person.name}
+      </li>
+      );
+    });
+  }
+
+  swapiService = new SwapiService();
+
+  componentDidMount(){
+    this.swapiService
+            .getAllPeople()
+            .then(peopleList => {
+              this.setState({peopleList : peopleList})
+            })
+  }
+
   render() {
+    const { peopleList } = this.state;
+    if(!peopleList){
+      return <Spiner/>;
+    }
+    const data = this.renderItems(peopleList);
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {data}
       </ul>
     );
   }
